8ba54f8f3ab8f23d2bb93bc0619811ffbfa63896
schiegl <schiegl.adrian@gmail.com>
2020-11-21 21:04:02 +0100

    add language resource download

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# download language resources
+snips-nlu download en
+
 # generate dataset
 snips-nlu generate-dataset en nlu-dataset.yaml > /tmp/nlu-dataset.json 
 
 # run bot
-python main.py "$DISCORD_API_TOKEN" --nlu-dataset /tmp/nlu-dataset.json --logfile bot.log
+python main.py $DISCORD_API_TOKEN --nlu-dataset /tmp/nlu-dataset.json --logfile bot.log
